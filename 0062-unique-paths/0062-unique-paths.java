20e9fde2a95d44540ed461306f5bfacc8fb6a274
Sandhya46-ux <sandhyakumari5862@gmail.com>
2026-09-10 12:45:30 +0530

    Time: 0 ms (100%), Space: 42 MB (81.86%) - LeetHub

--- 0062-unique-paths/0062-unique-paths.java
+++ 0062-unique-paths/0062-unique-paths.java
@@ -1,34 +1,74 @@
 class Solution {
-    
-//MEMOIZATION
-    public int uniquePaths(int m,int n ) {
-        // int m=grid.length;
-        // int n= grid[0].length;
-        ArrayList<ArrayList<Integer>> dp=new ArrayList<>();
-        for(int i=0;i<m;i++){
-            ArrayList<Integer> row=new ArrayList<>();
-            for(int j=0;j<n;j++){
-                row.add(-1);
-            }
-            dp.add(row);
-        }
-        return uniquePathsSum(m - 1, n - 1,dp);
-    }
 
-    public int uniquePathsSum(int i, int j,ArrayList<ArrayList<Integer>>dp) {
-        if (i == 0 && j == 0) {
-            return 1;
+//Recursion
+    // public int minPathSum(int i,int j) {
+
+//         return minPathSumRec(m - 1, n - 1);
+//     }
+//
+//     public int minPathSumRec(int i, int j) {
+//         if (i == 0 && j == 0) {
+//             return 1;
+//         }
+//         if (i < 0 || j < 0) {
+//             return 0;
+//         }
+//         int up = minPathSumRec(i - 1, j);
+//         int left = minPathSumRec(i, j - 1);
+//         return up+left);
+//     }
+
+//MEMOIZATION
+//     public int uniquePaths(int m,int n ) {
+//         ArrayList<ArrayList<Integer>> dp=new ArrayList<>();
+//         for(int i=0;i<m;i++){
+//             ArrayList<Integer> row=new ArrayList<>();
+//             for(int j=0;j<n;j++){
+//                 row.add(-1);
+//             }
+//             dp.add(row);
+//         }
+//         return uniquePathsSum(m - 1, n - 1,dp);
+//     }
+
+//     public int uniquePathsSum(int i, int j,ArrayList<ArrayList<Integer>>dp) {
+//         if (i == 0 && j == 0) {
+//             return 1;
+//         }
+//         if (i < 0 || j < 0) {
+//             return 0;
+//         }
+//         if(dp.get(i).get(j)!=-1){
+//             return dp.get(i).get(j);
+//         }
+//         int up = uniquePathsSum(i - 1, j,dp);
+//         int left = uniquePathsSum(i, j - 1,dp);
+//         int ans= up+left;
+//         dp.get(i).set(j,ans);
+//         return ans;
+//     }
+// }
+
+
+
+
+    // Tabulation
+    public int uniquePaths(int m, int n) {
+       int[][] dp=new int[m][n];
+        for (int i = 0; i < m; i++) {
+            dp[i][0] = 1;
         }
-        if (i < 0 || j < 0) {
-            return 0;
+         for (int j = 0; j < n; j++) {
+            dp[0][j] = 1;
         }
-        if(dp.get(i).get(j)!=-1){
-            return dp.get(i).get(j);
+        
+        for (int i = 1; i < m; i++) {
+            for (int j = 1; j < n; j++) {
+                int up = dp[i-1][j];
+                int left = dp[i][j-1]; 
+                dp[i][j] = up + left;
+            }
         }
-        int up = uniquePathsSum(i - 1, j,dp);
-        int left = uniquePathsSum(i, j - 1,dp);
-        int ans= up+left;
-        dp.get(i).set(j,ans);
-        return ans;
+        return dp[m-1][n-1];
     }
 }
