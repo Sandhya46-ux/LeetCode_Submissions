8bf316848c45015994fdc300a5f4d0c63600b0fd
Sandhya46-ux <sandhyakumari5862@gmail.com>
2026-09-16 22:28:53 +0530

    Time: 9 ms (54.38%), Space: 46.9 MB (23.18%) - LeetHub

--- 0494-target-sum/0494-target-sum.java
+++ 0494-target-sum/0494-target-sum.java
@@ -1,20 +1,32 @@
+import java.util.*;
 class Solution {
+    public int findTargetSumWays(int[] nums, int target) {
+        int n = nums.length;
+        int total = 0;
 
-    public int solve(int[] nums, int i, int current, int target) {
+        for (int i = 0; i < n; i++) {
+            total += nums[i];
+        }
+        int offset = total;
+        int[][] dp = new int[n + 1][2 * total + 1];
+        for (int[] row : dp) {
+            Arrays.fill(row, -1);
+        }
+        return memo(nums, 0, 0, target, offset, dp);
+    }
+    public int memo(int[] nums, int i, int current,int target, int offset, int[][] dp) {
         if (i == nums.length) {
             if (current == target) {
                 return 1;
-            } else {
-                return 0;
             }
+            return 0;
         }
-        int plus = solve(nums, i + 1, current + nums[i], target);
-        int minus = solve(nums, i + 1, current - nums[i], target);
-
-        return plus + minus;
-    }
-
-    public int findTargetSumWays(int[] nums, int target) {
-        return solve(nums, 0, 0, target);
+        int index = current + offset;
+        if (dp[i][index] != -1) {
+            return dp[i][index];
+        }
+        int plus = memo(nums, i + 1, current + nums[i],target, offset, dp);
+        int minus = memo(nums, i + 1, current - nums[i],target, offset, dp);
+        return dp[i][index] = plus + minus;
     }
 }
