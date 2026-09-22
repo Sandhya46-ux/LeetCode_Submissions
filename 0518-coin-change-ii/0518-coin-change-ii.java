128048ff6864a78412ef8311ab3301def9daa733
Sandhya46-ux <sandhyakumari5862@gmail.com>
2026-09-22 23:29:52 +0530

    Time: 12 ms (50.51%), Space: 56.7 MB (47.04%) - LeetHub

--- 0518-coin-change-ii/0518-coin-change-ii.java
+++ 0518-coin-change-ii/0518-coin-change-ii.java
@@ -17,49 +17,53 @@
 
 
 //Memoization
-class Solution {
-
-    public int change(int amount, int[] coins) {
-        int n = coins.length;
-        long[][] dp = new long[n + 1][amount + 1];
-        for (long[] row : dp) Arrays.fill(row, -1);
-        return (int) solve(n, amount, coins, dp);
-    }
-
-    private long solve(int i, int amount, int[] coins, long[][] dp) {
-        if (amount == 0) return 1;
-        if (i == 0) return 0;
-        if (amount < 0) return 0;
-        if (dp[i][amount] != -1) return dp[i][amount];
-
-        long pick = solve(i, amount - coins[i-1], coins, dp);
-        long notPick = solve(i -1, amount, coins, dp);
-
-        return dp[i][amount] = (pick + notPick);
-    }
-}
-
-
-
-
-// import java.util.*;
-
 // class Solution {
+
 //     public int change(int amount, int[] coins) {
 //         int n = coins.length;
-//         int[][] dp = new int[n + 1][amount + 1];
-//         for (int i = 0; i <= n; i++) {
-//             dp[i][0] = 1;
-//         }
-//         for (int i = 1; i <= n; i++) {
-//             for (int j = 1; j <= amount; j++) {
-//                 dp[i][j] = dp[i - 1][j];
-//                 if (coins[i - 1] <= j) {
-//                     dp[i][j] += dp[i][j - coins[i - 1]];
-//                 }
-//             }
-//         }
+//         long[][] dp = new long[n + 1][amount + 1];
+//         for (long[] row : dp) Arrays.fill(row, -1);
+//         return (int) solve(n, amount, coins, dp);
+//     }
 
-//         return dp[n][amount];
+//     private long solve(int i, int amount, int[] coins, long[][] dp) {
+//         if (amount == 0) return 1;
+//         if (i == 0) return 0;
+//         if (amount < 0) return 0;
+//         if (dp[i][amount] != -1) return dp[i][amount];
+
+//         long pick = solve(i, amount - coins[i-1], coins, dp);
+//         long notPick = solve(i -1, amount, coins, dp);
+
+//         return dp[i][amount] = (pick + notPick);
 //     }
 // }
+
+
+
+
+import java.util.*;
+
+class Solution {
+    public int change(int amount, int[] coins) {
+        int n = coins.length;
+        int[][] dp = new int[n + 1][amount + 1];
+        for (int i = 0; i <= n; i++) {
+            dp[i][0] = 1;
+        }
+        for(int j=0;j<=amount;j++){
+            dp[0][j]=0;
+        }
+        for (int i = 1; i <= n; i++) {
+            for (int j = 1; j <= amount; j++) {
+                int notPick=dp[i-1][j];
+                int pick=0;
+                if(coins[i-1]<=j){
+                    pick=dp[i][j-coins[i-1]];
+                }
+                dp[i][j]=pick+notPick;
+            }
+        }
+        return dp[n][amount];
+    }
+}
