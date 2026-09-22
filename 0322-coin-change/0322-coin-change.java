448d60dce9326f9af0f269d3cfd34aaf558e1076
Sandhya46-ux <sandhyakumari5862@gmail.com>
2026-09-22 10:00:58 +0530

    Time: 36 ms (11.79%), Space: 47.7 MB (8.21%) - LeetHub

--- 0322-coin-change/0322-coin-change.java
+++ 0322-coin-change/0322-coin-change.java
@@ -25,21 +25,21 @@ import java.util.*;
 
 class Solution {
     public int coinChange(int[] coins, int amount) {
-        int[][] dp = new int[coins.length][amount + 1];
+        int[][] dp = new int[coins.length+1][amount + 1];
         for (int[] row : dp){
             Arrays.fill(row, -1);
         }    
-        int result = solve(coins.length - 1, coins, amount, dp);
+        int result = solve(coins.length, coins, amount, dp);
         return result == Integer.MAX_VALUE ? -1 : result;
     }
     private int solve(int i, int[] coins, int amount, int[][] dp) {
         if (amount == 0) return 0;
-        if (i < 0 || amount < 0) return Integer.MAX_VALUE;
+        if (i <= 0 || amount < 0) return Integer.MAX_VALUE;
         if (dp[i][amount] != -1) return dp[i][amount];
 
         int pick = Integer.MAX_VALUE;
-        if (coins[i] <= amount) {
-            int res = solve(i, coins, amount - coins[i], dp);
+        if (coins[i-1] <= amount) {
+            int res = solve(i, coins, amount - coins[i-1], dp);
             if (res != Integer.MAX_VALUE) pick = res + 1;
         }
         int notPick = solve(i - 1, coins, amount, dp);
