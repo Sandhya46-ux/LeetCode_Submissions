621bcf94fc94233108531ae02daff6952bf4a029
Sandhya46-ux <sandhyakumari5862@gmail.com>
2026-09-22 09:58:07 +0530

    Time: 37 ms (10.45%), Space: 47.6 MB (10.37%) - LeetHub

--- 0322-coin-change/0322-coin-change.java
+++ 0322-coin-change/0322-coin-change.java
@@ -7,6 +7,7 @@
 //     private int solve(int i, int[] coins, int amount) {
 //         if (amount == 0) return 0;
 //         if (i < 0 || amount < 0) return Integer.MAX_VALUE;
+
 //         int pick = Integer.MAX_VALUE;
 //         if (coins[i] <= amount) {
 //             int res = solve(i, coins, amount - coins[i]);
@@ -17,29 +18,32 @@
 //     }
 // }
 
+
+
 //Memoization
 import java.util.*;
 
 class Solution {
     public int coinChange(int[] coins, int amount) {
-        int[] dp = new int[amount + 1];
-        Arrays.fill(dp, -1);
-        int result = solve(coins, amount, dp);
+        int[][] dp = new int[coins.length][amount + 1];
+        for (int[] row : dp){
+            Arrays.fill(row, -1);
+        }    
+        int result = solve(coins.length - 1, coins, amount, dp);
         return result == Integer.MAX_VALUE ? -1 : result;
     }
-
-    private int solve(int[] coins, int amount, int[] dp) {
+    private int solve(int i, int[] coins, int amount, int[][] dp) {
         if (amount == 0) return 0;
-        if (amount < 0) return Integer.MAX_VALUE;
-        if (dp[amount] != -1) return dp[amount];
-        int min = Integer.MAX_VALUE;
-        for (int i=0;i<coins.length;i++) {
-            int res = solve(coins, amount - coins[i], dp);
-            if (res != Integer.MAX_VALUE) {
-                min = Math.min(min, res + 1);
-            }
+        if (i < 0 || amount < 0) return Integer.MAX_VALUE;
+        if (dp[i][amount] != -1) return dp[i][amount];
+
+        int pick = Integer.MAX_VALUE;
+        if (coins[i] <= amount) {
+            int res = solve(i, coins, amount - coins[i], dp);
+            if (res != Integer.MAX_VALUE) pick = res + 1;
         }
-        dp[amount] = min;
-        return min;
+        int notPick = solve(i - 1, coins, amount, dp);
+        dp[i][amount] = Math.min(pick, notPick);
+        return dp[i][amount];
     }
 }
