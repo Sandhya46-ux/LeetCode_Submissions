c4bf965adb9351d54423be56b9d38d0da7081dbd
Sandhya46-ux <sandhyakumari5862@gmail.com>
2026-09-26 08:57:44 +0530

    Time: 27 ms (7.07%), Space: 54.3 MB (46.16%) - LeetHub

--- 0115-distinct-subsequences/0115-distinct-subsequences.java
+++ 0115-distinct-subsequences/0115-distinct-subsequences.java
@@ -1,33 +1,41 @@
 // class Solution {
 //     public int numDistinct(String s, String t) {
-//         return solve(s, t, 0, 0);
+//            int n=s.length();
+//            int m=t.length();
+//         return solve(s, t, n-1, m-1);
 //     }
 //     private int solve(String s, String t, int i, int j) {
-//         if (j == t.length()) return 1;
-//         if (i == s.length()) return 0;
-//         int ans = solve(s, t, i + 1, j);
+//         if (j <0) return 1;
+//         if (i <0) return 0;
+//         int ans = solve(s, t, i - 1, j);
 //         if (s.charAt(i) == t.charAt(j)) {
-//             ans += solve(s, t, i + 1, j + 1);
+//             ans += solve(s, t, i - 1, j -1);
 //         }
 //         return ans;
 //     }
 // }
 
 
+
+
+
+
 class Solution {
     public int numDistinct(String s, String t) {
+           int n=s.length();
+           int m=t.length();
         int[][] dp = new int[s.length()][t.length()];
         for (int[] row :dp) Arrays.fill(row,-1);
-        return solve(s,t,0,0,dp);
+        return solve(s,t,n-1,m-1,dp);
     }
     private int solve(String s, String t,int i,int j,int[][] dp) {
-        if (j == t.length()) return 1;
-        if (i == s.length()) return 0;
+        if (j<0) return 1;
+        if (i<0) return 0;
         if (dp[i][j] !=-1) return dp[i][j];
 
-        int ans = solve(s,t,i+1,j,dp);
+        int ans = solve(s,t,i-1,j,dp);
         if (s.charAt(i) == t.charAt(j)) {
-            ans += solve(s,t,i+1, j+1,dp);
+            ans += solve(s,t,i-1, j-1,dp);
         }
         dp[i][j] = ans;
         return ans;
